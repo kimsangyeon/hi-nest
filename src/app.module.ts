a32b37e40edd56bb0b07d6ca806b3632dce47528
kakao <luffy.yeon@kakaocorp.com>
2021-05-14 13:04:38 +0900

    #3 UNIT TESTING

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
-import { AppController } from './app/app.controller';
+import { AppController } from './app.controller';
 @Module({
   imports: [MoviesModule],
   controllers: [AppController], // express router 같은 존재
